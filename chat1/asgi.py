ade411cde6c0658467c4ba1279d959c24e3b0d01
Saurav-Chandel <chandelsaurav0817@gmail.com>
2022-05-04 17:59:25 +0530

    first commit

---
+++ chat1/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for chat1 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+import django
+from channels.http import AsgiHandler
+from chat1 import routing
+from channels.routing import ProtocolTypeRouter,URLRouter
+from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat1.settings')
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket":AuthMiddlewareStack(
+    URLRouter(routing.websocket_urlpatterns))
+})
